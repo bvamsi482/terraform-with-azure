a330c0830d417b6e6d66dd2efc0dd784878822e6
Vamsi <bvamsi482@gmail.com>
2019-12-31 10:47:58 +0530

    Modified Script

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ echo "Installing Azure CLI"
 
 apt-get update -y
 apt-get install -y sudo ca-certificates curl apt-transport-https lsb-release gnupg
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | 
-    gpg --dearmor | 
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+
 AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | 
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install azure-cli
+sudo apt-get install -y azure-cli
